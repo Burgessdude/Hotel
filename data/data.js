84b5ca2f53cb44f209bb9f66944ce08824f07ed1
Burgessdude <143647882+Burgessdude@users.noreply.github.com>
2024-11-07 16:34:47 -0700

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,50 @@
+export const hotels = [
+    {
+    "name":"The Plaza Hotel",
+    "photo":"plazanysmall.webp",
+    "address":["768 5th Ave,","New York, NY 10019"],
+    "phone":"212-759-3000"
+    },
+    {
+    "name":"The Algonquin Hotel",
+    "photo":"algonquinsmall.webp",
+    "address":["59 W 44th St,","New York, NY 10036"],
+    "phone":"212-840-6800"
+    },
+    {
+    "name":"Hotel Edison",
+    "photo":"hoteledisonsmall.webp",
+    "address":["228 W 47th St,","New York, NY 10036"],
+    "phone":"212-840-5000"
+    },
+    {
+    "name":"Hotel Elysee",
+    "photo":"hotelelyseesmall.webp",
+    "address":["60 E 54th St,","New York, NY 10022"],
+    "phone":"212-753-1066"
+    },
+    {
+    "name":"Ritz Carlton",
+    "photo":"ritzcarlton.webp",
+    "address":["50 Central Park S,","New York, NY 10019"],
+    "phone":"212-308-9100"
+    },
+    {
+    "name":"The St. Regis New York",
+    "photo":"stregissmall.webp",
+    "address":["Two E 55th St,","New York, NY 10022"],
+    "phone":"212-753-4500"
+    },
+    {
+    "name":"Waldorf Astoria",
+    "photo":"waldorfastoriasmall.webp",
+    "address":["301 Park Ave,","New York, NY 10022"],
+    "phone":"212-355-3000"
+    },
+    {
+    "name":"Washington Square",
+    "photo":"washingtonsquaresmall.webp",
+    "address":["103 Waverly Pl,","New York, NY 10011"],
+    "phone":"800-222-0418"
+    }   
+]
